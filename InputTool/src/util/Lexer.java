b22c3879c36ec5dafdb2f45d8f15b7cedabfc1c0
zengke.cai <zengke.cai@114.212.81.43>
2014-03-04 21:39:35 +0800

    delete function lexerAnalysis

--- InputTool/src/util/Lexer.java
+++ InputTool/src/util/Lexer.java
@@ -134,62 +134,6 @@ public class Lexer{
 		}
 	}
 
-
-	/**
-	 * function of lexical analysis, analysis sentence, store result into
-	 * tokenArray;
-	 */
-//	public static ArrayList<String> lexerAnalyse(String str){
-//		ArrayList<String> result = new ArrayList<String>();
-//		while (!str.equals("")){
-//			int index = 1;
-//
-//			//去除字符串头部的空白符
-//			if(str.substring(0, index).matches("[\\s]+")){
-//				str = str.substring(index);
-//				continue;
-//			}
-//
-//			if(isDelimiter(str.substring(0, index))){ // 分隔符
-//				result.add(str.substring(0, index));
-//				str = str.substring(index);
-//			}
-//			else if(str.startsWith("{")){ // 大括号
-//				//找到匹配的'}'的下标
-//				int count = 1;
-//				int i;
-//				for (i = 1; i < str.length(); i++){
-//					if(str.charAt(i) == '{')
-//						count++;
-//					if(str.charAt(i) == '}'){
-//						count--;
-//						if(count == 0)
-//							break;
-//					}
-//				}
-//				if(i < str.length()){
-//					result.add(str.substring(0, i + 1));
-//					str = str.substring(i + 1);
-//				}
-//				else{
-//					str = "";
-//				}
-//			}
-//			else if(isLegalToken(str.substring(0, index))){ //合法符号	TODO
-//				while (index <= str.length() && isLegalToken(str.substring(0, index)))
-//					index++;
-//
-//				result.add(str.substring(0, index - 1));
-//				str = str.substring(index - 1);
-//			}
-//			else{ //未定义的字符，直接添加进结果列表
-//				result.add(str.substring(0, index));
-//				str = str.substring(index);
-//			}
-//		}
-//
-//		return result;
-//	}
 	
 	
 	/**
